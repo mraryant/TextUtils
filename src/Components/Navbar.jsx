6ba5dd245321cba0caccf5e1a5a6229cd6791ac6
Aryant soni <87463187+mraryant@users.noreply.github.com>
2023-07-29 22:57:55 +0530

    Update Navbar.jsx word generator link updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar(props) {
                 <a className="nav-link" href="/https://replit.com/@mraryant/4-first-react-app#src/Components/About.jsx">About</a>
               </li> 
               <li className="nav-item">
-                <a className="nav-link" href="text-generator-chi.vercel.app">Word Generator </a>
+                <a className="nav-link" href="https://text-generator-chi.vercel.app/">Word Generator </a>
               </li> 
             </ul>
             
